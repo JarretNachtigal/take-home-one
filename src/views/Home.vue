1861e9afc818ec1cd40c5d3aa1abed049353cfb7
Jarret Nachtigal <jarretn1224@gmail.com>
2022-01-13 21:32:23 -0500

    initial commit and home.vue has text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Github Watcher Take Home</h1>
+    <h2>Frameworks:</h2>
+    <h3>Vue</h3>
+    <p><b>Stars:</b></p>
+    <p><b>Watchers:</b></p>
+    <p><b>Forks:</b></p>
+    <h3>Angular</h3>
+    <p><b>Stars:</b></p>
+    <p><b>Watchers:</b></p>
+    <p><b>Forks:</b></p>
+    <h3>Ember</h3>
+    <p><b>Stars:</b></p>
+    <p><b>Watchers:</b></p>
+    <p><b>Forks:</b></p>
+    <h3>Svelte</h3>
+    <p><b>Stars:</b></p>
+    <p><b>Watchers:</b></p>
+    <p><b>Forks:</b></p>
+    <h3>React</h3>
+    <p><b>Stars:</b></p>
+    <p><b>Watchers:</b></p>
+    <p><b>Forks:</b></p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
+  return: {},
+  created: {},
+  methods: {},
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
