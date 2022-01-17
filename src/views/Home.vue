af8ff73af0e31155357a752443d42612a0f43742
Jarret Nachtigal <jarretn1224@gmail.com>
2022-01-17 07:22:19 -0500

    framework laid out in home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,13 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <h1>Github Watcher Take Home</h1>
+    <h1>{{ test }}</h1>
     <h2>Frameworks:</h2>
     <h3>Vue</h3>
-    <p><b>Stars:</b></p>
+    <p>
+      <b>Stars:</b>
+      {{ vueStars }}
+    </p>
     <p><b>Watchers:</b></p>
     <p><b>Forks:</b></p>
     <h3>Angular</h3>
@@ -28,10 +32,21 @@
 
 <script>
 // @ is an alias to /src
+import axios from "axios";
 
 export default {
-  return: {},
-  created: {},
+  return: {
+    test: "",
+    vueStars: "",
+  },
+  created: function () {
+    axios.get("https://api.github.com/zen").then((response) => {
+      this.test = response.data;
+    });
+    axios.get("https://api.github.com/repos/vuejs/vue").then((response) => {
+      this.vueStars = response;
+    });
+  },
   methods: {},
   name: "Home",
   components: {},
